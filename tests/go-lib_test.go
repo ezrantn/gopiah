cd602e0c4d5a45950061aaa6c6a1a2b1b7439bb8
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-03 09:18:05 +0700

    test: unit test passed

---
+++ tests/go-lib_test.go
@@ -0,0 +1,24 @@
+package tests_test
+
+import (
+	"gopiah/lib"
+	"testing"
+)
+
+func TestConvertToRupiah(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected string
+	}{
+		{0, "Rp. 0,00"},
+		{12345, "Rp. 12.345,00"},
+		{123456789, "Rp. 123.456.789,00"},
+	}
+
+	for _, tc := range testCases {
+		result := lib.ConvertToRupiah(tc.input)
+		if result != tc.expected {
+			t.Errorf("For input %d, expected %s, but got %s", tc.input, tc.expected, result)
+		}
+	}
+}
